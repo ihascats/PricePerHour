65be4a0c1b745ddca9b032460415aafe12cf6589
ihascats <s.pavlovic.rs@gmail.com>
2022-12-09 23:51:42 +0100

    Clean up timesToBeat comp & make it match theme

--- components/timesToBeat.js
+++ components/timesToBeat.js
@@ -3,22 +3,29 @@ export default function TimesToBeat({ hltbSelected, price }) {
     hltbSelected;
   const { initial, final_formatted } = price;
   return (
-    <div className="grid grid-cols-2">
-      <p>Main: {gameplayMain}h</p>
-      <p>
-        PPH: {final_formatted[0]}
-        {(initial / (gameplayMain * 100)).toFixed(2)}
-      </p>
-      <p>Main + Extra: {gameplayMainExtra}h</p>
-      <p>
-        PPH: {final_formatted[0]}
-        {(initial / (gameplayMainExtra * 100)).toFixed(2)}
-      </p>
-      <p>Completionist: {hltbSelected.gameplayCompletionist}h</p>
-      <p>
-        PPH: {final_formatted[0]}
-        {(initial / (gameplayCompletionist * 100)).toFixed(2)}
-      </p>
+    <div className="flex p-2 bg-neutral-800 border-b-2 border-b-neutral-500 text-neutral-50 font-mono">
+      <div className="grid grid-cols-3 w-full whitespace-nowrap gap-x-2">
+        <p className="text-red-400 w-fit">Main:</p>
+        <p className="text-red-400">{gameplayMain}h</p>
+        <p className="text-red-400">
+          {final_formatted[0]}
+          {(initial / (gameplayMain * 100)).toFixed(2)}/h
+        </p>
+
+        <p className="text-yellow-400 w-fit">Main+:</p>
+        <p className="text-yellow-400">{gameplayMainExtra}h</p>
+        <p className="text-yellow-400">
+          {final_formatted[0]}
+          {(initial / (gameplayMainExtra * 100)).toFixed(2)}/h
+        </p>
+
+        <p className="text-green-400 w-fit">100%:</p>
+        <p className="text-green-400">{gameplayCompletionist}h</p>
+        <p className="text-green-400">
+          {final_formatted[0]}
+          {(initial / (gameplayCompletionist * 100)).toFixed(2)}/h
+        </p>
+      </div>
     </div>
   );
 }
