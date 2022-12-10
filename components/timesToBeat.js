858145b072fac1ff1d3a0a579beff5959bdd52c3
ihascats <s.pavlovic.rs@gmail.com>
2022-12-10 20:13:13 +0100

    Display N/A if a value isn't available

--- components/timesToBeat.js
+++ components/timesToBeat.js
@@ -6,21 +6,33 @@ export default function TimesToBeat({ hltbSelected, price }) {
     <div className="flex p-2 bg-neutral-800 border-b-2 border-b-neutral-500 text-neutral-50 font-mono">
       <div className="grid grid-cols-3 w-full whitespace-nowrap gap-x-2">
         <p className="text-red-400 w-fit">Main:</p>
-        <p className="text-red-400">{gameplayMain}h</p>
         <p className="text-red-400">
-          {(initial / (gameplayMain * 100)).toFixed(2)}/h
+          {gameplayMain ? `${gameplayMain}h` : 'N/A'}
+        </p>
+        <p className="text-red-400">
+          {initial && gameplayMain
+            ? `${(initial / (gameplayMain * 100)).toFixed(2)}/h`
+            : 'N/A'}
         </p>
 
         <p className="text-yellow-400 w-fit">Main+:</p>
-        <p className="text-yellow-400">{gameplayMainExtra}h</p>
         <p className="text-yellow-400">
-          {(initial / (gameplayMainExtra * 100)).toFixed(2)}/h
+          {gameplayMainExtra ? `${gameplayMainExtra}h` : 'N/A'}
+        </p>
+        <p className="text-yellow-400">
+          {initial && gameplayMainExtra
+            ? `${(initial / (gameplayMainExtra * 100)).toFixed(2)}/h`
+            : 'N/A'}
         </p>
 
         <p className="text-green-400 w-fit">100%:</p>
-        <p className="text-green-400">{gameplayCompletionist}h</p>
         <p className="text-green-400">
-          {(initial / (gameplayCompletionist * 100)).toFixed(2)}/h
+          {gameplayCompletionist ? `${gameplayCompletionist}h` : 'N/A'}
+        </p>
+        <p className="text-green-400">
+          {initial && gameplayCompletionist
+            ? `${(initial / (gameplayCompletionist * 100)).toFixed(2)}/h`
+            : 'N/A'}
         </p>
       </div>
     </div>
